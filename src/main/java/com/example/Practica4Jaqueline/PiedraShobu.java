d04900317bc172e80bfebbf64d1571e0055ffcd8
johab <johab.montoya@uabc.edu.mx>
2026-05-08 19:23:23 -0700

    modificaciones de clase PiedraShobu con getters, setters y modificación en jugador con un int que permite registrar su id

--- src/main/java/com/example/Practica4Jaqueline/PiedraShobu.java
+++ src/main/java/com/example/Practica4Jaqueline/PiedraShobu.java
@@ -3,6 +3,18 @@ package com.example.Practica4Jaqueline;
 public class PiedraShobu {
     private int posX;
     private int posY;
-    private String jugador;
+    private int jugadorId; // 0 o 1
 
+    public PiedraShobu(int posX, int posY, int jugadorId) {
+        this.posX = posX;
+        this.posY = posY;
+        this.jugadorId = jugadorId;
+    }
+
+    public int getPosX() { return posX; }
+    public int getPosY() { return posY; }
+    public int getJugadorId() { return jugadorId; }
+
+    public void setPosX(int posX) { this.posX = posX; }
+    public void setPosY(int posY) { this.posY = posY; }
 }
