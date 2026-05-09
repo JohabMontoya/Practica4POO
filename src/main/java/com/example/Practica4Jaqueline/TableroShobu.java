8d9f8b4579d09610e6b12f551154df4dbbae3766
johab <johab.montoya@uabc.edu.mx>
2026-05-08 20:24:26 -0700

    Se agrega método para colocar una ficha en un lugar diferente.

--- src/main/java/com/example/Practica4Jaqueline/TableroShobu.java
+++ src/main/java/com/example/Practica4Jaqueline/TableroShobu.java
@@ -21,6 +21,24 @@ public class TableroShobu {
         return get(x, y) == null;
     }
 
+    /**
+     * Este método verifica que puedas colocar una piedra revisando
+     * si la piedra existe, si está dentro del tablero y si la casilla
+     * existe, modifica la posición de la piedra.
+     */
+
+    public boolean colocar(PiedraShobu p, int x, int y) {
+        if (p == null) return false;
+        if (!dentro(x, y)) return false;
+        if (!estaVacio(x, y)) return false;
+
+
+        piedra[x][y] = p;
+        p.setPosX(x);
+        p.setPosY(y);
+        return true;
+    }
+
     public int getAncho() { return 4; }
     public int getAlto()  { return 4; }
 }
