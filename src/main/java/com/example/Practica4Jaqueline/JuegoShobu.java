359be1737bbd069e759cfbba32f5ef17cdb87903
johab <johab.montoya@uabc.edu.mx>
2026-05-08 18:02:58 -0700

    Atributos de juego shobu

--- src/main/java/com/example/Practica4Jaqueline/JuegoShobu.java
+++ src/main/java/com/example/Practica4Jaqueline/JuegoShobu.java
@@ -1,8 +1,12 @@
 package com.example.Practica4Jaqueline;
 
 public class JuegoShobu {
-    public TableroShobu[] tableros;
-    public JugadorShobu[] jugadores;
+    // Atributos.
+    public TableroShobu[] tableroHome = new TableroShobu[2];
+    public TableroShobu[] tablerosAway = new TableroShobu[2];
+    public JugadorShobu[] jugadores = new JugadorShobu[2];
+    public PiedraShobu[][] piedras = new PiedraShobu[4][4];
+
     public int jugadorActual;
 
 
