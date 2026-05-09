9776db15c687eda569016e142a7cd2b333f43b99
johab <johab.montoya@uabc.edu.mx>
2026-05-08 20:17:50 -0700

    Tablero agrega metodos para verificar si está dentro de él, para saber posición, para verificar si la casilla está vacía y para regreasar el tamaño del TableroShobu.

--- src/main/java/com/example/Practica4Jaqueline/TableroShobu.java
+++ src/main/java/com/example/Practica4Jaqueline/TableroShobu.java
@@ -1,7 +1,26 @@
 package com.example.Practica4Jaqueline;
 
 public class TableroShobu {
-    private PiedraShobu[][] Fichas;
 
+    private PiedraShobu[][] piedra;
 
+    public TableroShobu() {
+        this.piedra = new PiedraShobu[4][4];
+    }
+
+    public boolean dentro(int x, int y) {
+        return x >= 0 && x < 4 && y >= 0 && y < 4;
+    }
+
+    public PiedraShobu get(int x, int y) {
+        if (!dentro(x, y)) return null;
+        return piedra[x][y];
+    }
+
+    public boolean estaVacio(int x, int y) {
+        return get(x, y) == null;
+    }
+
+    public int getAncho() { return 4; }
+    public int getAlto()  { return 4; }
 }
